99ca4fd9e99218908cdb8f33f4ce7c9c72b1f3f4
𒋲𝑴𝑨𝑺𝑻𝑬𝑹𝑴𝑰𝑵𝑫𒋲 <hadeslord087@gmail.com>
2025-02-16 21:43:33 +0000

    Add files via upload

--- settings/cfg.js
+++ settings/cfg.js
@@ -1,1 +1,15 @@
+global.owner = [
+  "2250565647864", //ganti nomor owner
+  "2250565647864" //nomor owner kedua kalo ada
+]
+global.nomorbot = '2250565647864'
+global.urlfoto = 'https://imgur.com/a/PjlJHxy'
 
+let fs = require('fs')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file)
+})
